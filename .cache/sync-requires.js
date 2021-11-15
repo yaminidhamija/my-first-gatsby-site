5d554e36d98ccbef7902b0a46b7e07613752523b
Yamini Dhamija <ydhamija@tillster.com>
2021-11-15 18:16:12 +0530

    initial commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/yaminidhamija/jamstack/my-first-gatsby-site/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/yaminidhamija/jamstack/my-first-gatsby-site/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/yaminidhamija/jamstack/my-first-gatsby-site/src/pages/index.js"))
+}
+
